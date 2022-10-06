352da1bd9de0cf5fe1a7290c485bf0975cd6e2af
Lucas Lernoud <lucaslerdb@gmail.com>
2022-10-06 17:10:28 +0200

    sign in avec jwt

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import authRoutes from "./routes/auth.js"
 import userRoutes from "./routes/users.js"
 import videoRoutes from "./routes/videos.js"
 import commentRoutes from "./routes/comments.js"
-import nodemon from "nodemon";
+import cookieParser from "cookie-parser";
 
 
 const app = express();
@@ -17,7 +17,7 @@ const connect = () => {
   mongoose
     .connect(process.env.PASSWORDDB)
     .then(() => {
-      console.log('connected to db');
+      console.log('connected to database');
     })
     .catch((err) => {
       throw err;
@@ -28,11 +28,23 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.use(cookieParser())
 app.use(express.json())
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/videos", videoRoutes)
 app.use("/api/comments", commentRoutes)
+
+
+app.use((err, req,res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong";
+  return res.status(status).json({
+    success: false,
+    status,
+    message
+  })
+})
 
 app.listen(port, () => {
   connect();
