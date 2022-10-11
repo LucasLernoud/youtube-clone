b7793bb9738430c64ea1b9826488255a79ec1b4e
Lucas Lernoud <lucaslerdb@gmail.com>
2022-10-11 17:30:37 +0200

    controllers video

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,17 @@
 import express from "express"
-import  {test}  from "../controllers/user.js";
+import  {updateUser, deleteUser, getUser, subscribe, unsubscribe, like, dislike}  from "../controllers/user.js";
+import {verifyToken} from "../verifyToken.js"
+
 
 const router = express.Router();
 
-router.get("/test", test)
+router.put("/:id", verifyToken, updateUser);
+router.delete("/:id", verifyToken, deleteUser )
+router.get("/find/:id", getUser)
+router.put("/sub/:id", verifyToken , subscribe)
+router.put("/unsub/:id", verifyToken, unsubscribe)
+router.put("/like/:id", verifyToken , like)
+router.put("/dislike/:id", verifyToken, dislike)
+
 
 export default router;
